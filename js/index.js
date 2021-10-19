2bfb5f3b91bd79ff0398bea805043898fcbdbe6d
FabianoEtec <nbscbs@gmail.com>
2021-10-19 19:01:19 -0300

    Finalizado

---
+++ js/index.js
@@ -0,0 +1,56 @@
+function mostrar(){
+    let n5 = parseInt(document.querySelector('#n5').value);
+    let tabuada='';
+   
+    if(isNaN(n5)){
+        
+        document.querySelector('#hmedia4').innerHTML = 'Digite um número - ' +  '<span>' + ' V A L I D O ! ' + '</span>';
+    }else{
+        document.querySelector('#hmedia4').innerHTML = 'Tabuada do ' + ' '+ ' <span> ' + n5 + ' </span> ';
+    }
+    
+    if(!isNaN(n5)) {    
+       
+    
+    for(let i=1; i<=10 ; i++){
+        tabuada += n5 + ' x ' + i + ' = ' + '<p>' + n5 * i + '</p>' + '<br>';
+    }
+} 
+    
+    document.querySelector('#resTabuada').innerHTML = tabuada;
+    
+  }
+
+function calcular(){
+    let n1 = parseInt(document.querySelector('#n1').value);
+    let n2 = parseInt(document.querySelector('#n2').value);
+    if(isNaN(n1)|| isNaN(n2)){
+        alert('Digite um número VALIDO!');
+    }else{
+    document.querySelector('#res').innerHTML = n1 + n2;
+    }
+}
+//calculaMedia
+function calculaMedia(){
+    let nota1 = parseInt(document.querySelector('#nota1').value);
+    let nota2 = parseInt(document.querySelector('#nota2').value);
+    let nota3 = parseInt(document.querySelector('#nota3').value);
+    let nota4 = parseInt(document.querySelector('#nota4').value);
+
+    let media = (nota1 + nota2 + nota3 + nota4) / 2;
+
+    if(isNaN(nota1)|| isNaN(nota2)||isNaN(nota3)||isNaN(nota4)){
+        alert('Digite um número VALIDO!');
+    }
+    
+    else{
+    document.querySelector('.resmedia').innerHTML = media; 
+    }
+    if(media >= 5 || media < 6){
+        document.querySelector('.h2media').innerHTML = 'Aluno de  ' +  '<span>' + ' R E C U P E R A Ç Ã O  ! ' + '</span>';
+    }if(media >= 6){
+        document.querySelector('.h2media').innerHTML = 'Aluno ' +  '<span>' + ' A P R O V A D O  ! ' + '</span>';
+    }else{
+        document.querySelector('.h2media').innerHTML = 'Aluno ' +  '<span>' + ' R E P R O V A D O  ! ' + '</span>';
+    }
+}
